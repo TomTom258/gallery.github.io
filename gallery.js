3c10fc15137cff24f69526f1ee884d4a30a49cd0
TomTom258 <108875773+TomTom258@users.noreply.github.com>
2022-10-27 18:18:37 +0200

    Created a webpage from the gallery project

---
+++ gallery.js
@@ -0,0 +1,123 @@
+const images = [
+    {
+        "src": "https://www.techgrapple.com/wp-content/uploads/2016/08/Mountain-lake-nature-background.jpg",
+        "title": "A Bay",
+        "description": "A beuatiful picture of a bay and a Mountain next to it"
+    },
+    {
+        "src": "https://www.techgrapple.com/wp-content/uploads/2016/08/Nature-Wallpaper-Sky.jpg",
+        "title": "A Mountain",
+        "description": "A beuatiful night photo of a mountain"
+    },
+    {
+        "src": "https://www.techgrapple.com/wp-content/uploads/2016/08/Boat-Lake-Wallpaper.jpg",
+        "title": "A little boat",
+        "description": "A picture of a small probably fishing boat docked somewhere in nature"
+    },
+    {
+        "src": "https://www.techgrapple.com/wp-content/uploads/2016/08/Nature-Wallpaper-Cool-building.jpg",
+        "title": "A Japanese house",
+        "description": "A picture of a Japanese house somewhere in a forest"
+    },
+    {
+        "src": "https://www.techgrapple.com/wp-content/uploads/2016/08/nature-evening-sunset-wallpaper.jpg",
+        "title": "A beuatiful sunset",
+        "description": "A picture of a sunset over the lake"
+    }
+]
+
+let preview = document.querySelector('.preview');
+let background = document.getElementById('background');
+let right = document.getElementById('right');
+let left = document.getElementById('left');
+let title = document.querySelector('.title');
+let description = document.querySelector('.description');
+let index = 0;
+
+title.innerText = images[index].title;
+description.innerText = images[index].description;
+
+if (images.length > 5) {
+
+    for (i = 0; i < 4; i++) {
+        let newImg = document.createElement('img');
+
+        newImg.setAttribute('src', images[i].src);
+        newImg.setAttribute('class', 'thumbnail');
+        preview.appendChild(newImg);
+    }
+}
+
+else {
+    
+    for (i = 0; i < images.length; i++) {
+        let newImg = document.createElement('img');
+
+        newImg.setAttribute('src', images[i].src);
+        newImg.setAttribute('class', 'thumbnail');
+        preview.appendChild(newImg);
+    } 
+}
+
+let thumbnails = document.querySelectorAll('.thumbnail');
+
+right.onclick = () => {
+    let newSrc;
+    let newTitle;
+    let newDescription;
+
+    if (index === images.length - 1) {
+        index = 0;
+        newTitle = images[index].title;
+        title.innerText = newTitle;
+        newDescription = images[index].description;
+        description.innerText = newDescription;
+        newSrc = thumbnails[index].getAttribute('src');
+        thumbnails[images.length - 1].setAttribute('class', 'thumbnail');
+        thumbnails[index].setAttribute('class', 'thumbnail highlight');
+        background.style.backgroundImage = `url("${newSrc}")`;
+    }
+
+    else {
+        index++;
+        newTitle = images[index].title;
+        title.innerText = newTitle;
+        newDescription = images[index].description;
+        description.innerText = newDescription;
+        newSrc = thumbnails[index].getAttribute('src');
+        thumbnails[index - 1].setAttribute('class', 'thumbnail');
+        thumbnails[index].setAttribute('class', 'thumbnail highlight');
+        background.style.backgroundImage = `url("${newSrc}")`;
+    }
+}
+
+left.onclick = () => {
+    let newSrc;
+    let newTitle;
+    let newDescription;
+
+
+    if (index - 1 < 0) {
+        index = images.length - 1;
+        newTitle = images[index].title;
+        title.innerText = newTitle;
+        newDescription = images[index].description;
+        description.innerText = newDescription;
+        newSrc = thumbnails[index].getAttribute('src');
+        thumbnails[index].setAttribute('class', 'thumbnail highlight');
+        thumbnails[0].setAttribute('class', 'thumbnail');
+        background.style.backgroundImage = `url("${newSrc}")`;
+    }
+
+    else {
+        index--;
+        newTitle = images[index].title;
+        title.innerText = newTitle;
+        newDescription = images[index].description;
+        description.innerText = newDescription;
+        newSrc = thumbnails[index].getAttribute('src');
+        thumbnails[index].setAttribute('class', 'thumbnail highlight');
+        thumbnails[index + 1].setAttribute('class', 'thumbnail');
+        background.style.backgroundImage = `url("${newSrc}")`;
+    }
+}
